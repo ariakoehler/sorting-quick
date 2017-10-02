c65ce14ced2c656255aac92cf5bf21849b23f174
Carter Koehler <carterleekoehler@gmail.com>
2017-10-02 15:05:25 -0500

    rng_nums changes

--- rng_nums.py
+++ rng_nums.py
@@ -8,7 +8,14 @@ pi=np.pi; e=np.e
 if __name__=='__main__':
     
     file_out = open('numbers.txt', 'w')
-    
-    for i in range(50000):
-        randomInt = np.random.randint(-20000, 20000)
-        file_out.write('{}\n'.format(randomInt))
+
+    num_words = 50
+
+    file_out.write('{}\n{}\n'.format(num_words, num_words))
+
+    for i in range(50):
+        alpha = list('qwertyuiop[]\asdfghjkl;zxcvbnm,./0123645789')
+        np_alpha = np.array(alpha)
+        random_word = str(np.random.choice(np_alpha, 30))
+        
+        file_out.write('{}\n'.format(random_word))
